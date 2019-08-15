7071f1c966113a86ea4c676f5e8815fc8302a99c
KIRIN3 <scomputer13@gmail.com>
2019-08-15 23:21:45 +0900

    Initial commit

---
+++ app/src/main/java/kirin3/jp/viewmodel_livedata_databinding/MainActivity.kt
@@ -0,0 +1,12 @@
+package kirin3.jp.viewmodel_livedata_databinding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
