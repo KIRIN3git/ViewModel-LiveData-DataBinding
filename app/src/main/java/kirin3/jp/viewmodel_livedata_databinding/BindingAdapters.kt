31e3b44a23570e3d6e24d61892d99a694705704c
KIRIN3 <scomputer13@gmail.com>
2019-08-17 19:06:36 +0900

    調整

--- app/src/main/java/kirin3/jp/viewmodel_livedata_databinding/BindingAdapters.kt
+++ app/src/main/java/kirin3/jp/viewmodel_livedata_databinding/BindingAdapters.kt
@@ -1,9 +1,7 @@
 package kirin3.jp.viewmodel_livedata_databinding
 
-import android.content.res.ColorStateList
 import android.databinding.BindingAdapter
 import android.support.v4.content.ContextCompat
-import android.support.v4.widget.ImageViewCompat
 import android.widget.ImageView
 
 @BindingAdapter("app:logoIcon")
