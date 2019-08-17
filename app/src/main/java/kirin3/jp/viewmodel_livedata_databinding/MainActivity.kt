0611c5b692ee790b16c415321028dd63b98ad69e
KIRIN3 <scomputer13@gmail.com>
2019-08-17 18:40:12 +0900

    調整

--- app/src/main/java/kirin3/jp/viewmodel_livedata_databinding/MainActivity.kt
+++ app/src/main/java/kirin3/jp/viewmodel_livedata_databinding/MainActivity.kt
@@ -4,9 +4,6 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import kirin3.jp.viewmodel_livedata_databinding.R
-import kirin3.jp.viewmodel_livedata_databinding.UserViewModel
 import kirin3.jp.viewmodel_livedata_databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 何度も使いまわされるインスタンスを発行
+        // インスタンスを発行
         val viewModel: UserViewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
 
         // Bindingオブジェクトを生成する（onCreateの外でもよい）
